39d13e026a6dca4ada70145b8ed8e424523b75ed
Akash <akash.kahalkar.08@gmail.com>
2020-06-22 06:32:10 -0700

    added haptic feedback

--- Test_animations/ContentView.swift
+++ Test_animations/ContentView.swift
@@ -8,13 +8,30 @@
 
 import SwiftUI
 
+struct SliderView: View {
+    var title: String
+    @Binding var sliderValue: CGFloat
+    var minRange: CGFloat
+    var maxRange: CGFloat
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+            HStack {
+                Slider(value: $sliderValue, in: minRange...maxRange, step: 1)
+                Text(String(Int(sliderValue))) // do I really need to do this?
+            }
+        }.padding()
+    }
+}
+
 struct ContentView: View {
     //shape parameter
     @State var tapped = false
-    @State var shapeRadius = 236.0
-    @State var shapeSize = 100.0
-    @State var shadowOffset = 7.0
-    @State var shadowBlur = 10.0
+    @State var shapeRadius: CGFloat = 0.0
+    @State var shapeSize: CGFloat = 100.0
+    @State var shadowOffset: CGFloat = 7.0
+    @State var shadowBlur: CGFloat = 10.0
     
     //color slider parameters
     @State private var isDragging: Bool = false
@@ -24,9 +41,9 @@ struct ContentView: View {
     @State var chosenColor: Color  = Color(#colorLiteral(red: 0.5077621341, green: 0.05276752263, blue: 0, alpha: 1))
     private var linearGradientWidth: CGFloat = 300
     private var colors: [Color] = {
-        let hueValues = Array(0...359)
+        let hueValues = Array(0...255)
         return hueValues.map {
-            Color(UIColor(hue: CGFloat($0) / 359.0 ,
+            Color(UIColor(hue: CGFloat($0) / 255.0 ,
                           saturation: 1.0,
                           brightness: 1.0,
                           alpha: 1.0))
@@ -47,15 +64,17 @@ struct ContentView: View {
                         .foregroundColor(self.chosenColor)
                         .frame(width: CGFloat(self.shapeSize), height: CGFloat(self.shapeSize))
                         .clipShape(RoundedRectangle(cornerRadius: CGFloat(self.shapeRadius)))
-                        .padding(.top, 50)                            //top shadow
+                        .padding(.top, 50)
+                        //top shadow
                         .shadow(color: self.currentColorLight.opacity(0.3),
-                                radius: CGFloat(self.shadowBlur), x: -CGFloat(self.shadowOffset), y: -CGFloat(self.shadowOffset))
+                                radius: CGFloat(self.shadowBlur), x: -self.getShadowOffset(value: shadowOffset), y: -self.getShadowOffset(value: shadowOffset))
                         //bottopm shadow
                         .shadow(color: Color.black.opacity(0.3),
-                                radius: CGFloat(self.shadowBlur), x: CGFloat(self.shadowOffset), y: CGFloat(self.shadowOffset))
+                                radius: CGFloat(self.shadowBlur), x: self.getShadowOffset(value: shadowOffset), y: self.getShadowOffset(value: shadowOffset))
                         .onTapGesture {
+                            UIImpactFeedbackGenerator(style: .medium).impactOccurred()
                             self.tapped.toggle()
-                    }.animation(.easeIn)
+                    }.animation(.easeInOut)
                     Spacer()
                     ZStack(alignment: .leading) {
                         Rectangle().fill(LinearGradient(gradient: Gradient(colors: self.colors),
@@ -84,8 +103,8 @@ struct ContentView: View {
                     ZStack {
                         RoundedRectangle(cornerRadius: 10).frame(height: 60).padding(8)
                             .foregroundColor(self.chosenColor)
-                            .shadow(color:self.currentColorLight.opacity(0.3), radius: 5, x: -5, y: -5)
-                            .shadow(color: Color.black.opacity(0.3), radius: 5, x: 5, y: 5)
+                            .shadow(color:self.currentColorLight.opacity(0.3), radius: 3, x: -3, y: -3)
+                            .shadow(color: Color.black.opacity(0.3), radius: 3, x: 3, y: 3)
                         
                         Picker(selection: self.$pickerValue, label: Text("Shape style picker")) {
                             Text(self.shapeStyle[0]).tag(0)
@@ -95,42 +114,19 @@ struct ContentView: View {
                         .pickerStyle(SegmentedPickerStyle())
                         .padding()
                     }
-                    VStack(alignment: .leading) {
-                        Text("Radius")
-                        HStack {
-                            Slider(value: self.$shapeRadius, in: 0...100, step: 1)
-                            Text(String(self.shapeRadius))
-                        }
-                    }.padding()
-                    VStack(alignment: .leading) {
-                        Text("Size")
-                        HStack {
-                            Slider(value: self.$shapeSize, in: 10...200, step: 1)
-                            Text(String(self.shapeSize))
-                        }
-                    }.padding()
                     
-                    VStack(alignment: .leading) {
-                        Text("Shadow offset")
-                        HStack {
-                            Slider(value: self.$shadowOffset, in: 0...30, step: 1)
-                            Text(String(self.shadowOffset))
-                        }
-                    }.padding()
-                    
-                    VStack(alignment: .leading) {
-                        Text("Shadow Blur")
-                        HStack {
-                            Slider(value: self.$shadowBlur, in: 0...50, step: 1)
-                            Text(String(self.shadowBlur))
-                        }
-                    }.padding()
+                    SliderView(title: "Radius", sliderValue: $shapeRadius, minRange: 0, maxRange: 100)
+                    SliderView(title: "Size", sliderValue: $shapeSize, minRange: 10, maxRange: 200)
+                    SliderView(title: "Shadow offset", sliderValue: $shadowOffset, minRange: 0, maxRange: 30)
+                    SliderView(title: "Shadow Blur", sliderValue: $shadowBlur, minRange: 0, maxRange: 50)
                 }
             }
         }
     }
     
-    /// 1
+    private func getShadowOffset(value: CGFloat) -> CGFloat {
+        self.tapped ? -value : value
+    }
     /// Get the current color based on our current translation within the view
     private var currentColor: Color {
         Color(UIColor.init(hue: self.normalizeGesture() / linearGradientWidth, saturation: 1.0, brightness: 0.5, alpha: 1.0))
@@ -140,15 +136,14 @@ struct ContentView: View {
         Color(UIColor.init(hue: self.normalizeGesture() / linearGradientWidth, saturation: 1.0, brightness: 1.0, alpha: 1.0))
     }
     
-    /// 2
-    /// Normalize our gesture to be between 0 and 200, where 200 is the height.
-    /// At 0, the users finger is on top and at 200 the users finger is at the bottom
+    ///
     private func normalizeGesture() -> CGFloat {
         let offset = startLocation + dragOffset.width // Using our starting point, see how far we've dragged +- from there
-        let maxY = max(0, offset) // We want to always be greater than 0, even if their finger goes outside our view
-        let minY = min(maxY, linearGradientWidth) // We want to always max at 200 even if the finger is outside the view.
+        // We want to always be greater than 0, even if their finger goes outside our view
+        // We want to always max at 200 even if the finger is outside the view.
+        return min(max(0, offset), linearGradientWidth)
         
-        return minY
+        
     }
     
     private func normalizedX() -> CGFloat {
